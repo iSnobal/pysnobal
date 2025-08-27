98c9dd7b3aa38f83feb52035f77e096f61b0380e
Joachim Meyer <jmeyer@boisestate.edu>
2025-08-27 12:23:20 -0600

    Setup - Update to use pyproject.toml and latest setuptools
    Addresses deprecation warnings raised during install being way behind in time.

--- setup.py
+++ setup.py
@@ -1,86 +1,46 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
 import glob
+import os
 import sys
+
 import numpy
-
-from setuptools import setup, find_packages, Extension
-
-# from distutils.extension import Extension
+from setuptools import setup, Extension
 from Cython.Distutils import build_ext
 
-with open('README.md') as readme_file:
-    readme = readme_file.read()
+# Allow user to specify a compiler; default to gcc if not provided
+os.environ.setdefault("CC", "gcc")
 
-requirements = [
-    # TODO: put package requirements here
-]
-
-test_requirements = [
-    # TODO: put package test requirements here
-]
-
-# make sure we're using GCC
-if "CC" not in os.environ:
-    os.environ["CC"] = "gcc"
-
-if sys.platform == 'darwin':
+# macOS: adjust linker flags for shared libs
+if sys.platform == "darwin":
     from distutils import sysconfig
     vars = sysconfig.get_config_vars()
-    vars['LDSHARED'] = vars['LDSHARED'].replace('-bundle', '-dynamiclib')
+    if vars.get("LDSHARED"):
+        vars["LDSHARED"] = vars["LDSHARED"].replace("-bundle", "-dynamiclib")
 
-# ------------------------------------------------------------------------------
-# Compiling the C code for the Snobal library
+# Collect sources
+c_sources = glob.glob("pysnobal/c_snobal/libsnobal/*.c")
+pyx_source = "pysnobal/c_snobal/snobal.pyx"
+sources = c_sources + [pyx_source]
 
-loc = 'pysnobal/c_snobal/libsnobal'
-cwd = os.getcwd()
-sources = glob.glob(os.path.join(loc, '*.c'))
+extra_cc_args = ["-fopenmp", "-O3", "-L./pysnobal"]
 
-loc = 'pysnobal/c_snobal'
-extra_cc_args = ['-fopenmp', '-O3', '-L./pysnobal']
-sources += [os.path.join(loc, val) for val in ["snobal.pyx"]]
 extensions = [
     Extension(
         "pysnobal.c_snobal.snobal",
-        sources,
-        # libraries=["snobal"],
+        sources=sources,
         include_dirs=[
             numpy.get_include(),
-            'pysnobal/c_snobal',
-            'pysnobal/c_snobal/h'
+            "pysnobal/c_snobal",
+            "pysnobal/c_snobal/h",
         ],
-        # runtime_library_dirs=['{}'.format(os.path.join(cwd,'pysnobal'))],
         extra_compile_args=extra_cc_args,
         extra_link_args=extra_cc_args,
     )
 ]
 
 setup(
-    name='pysnobal',
-    description="Python wrapper of the Snobal mass and "
-                "energy balance snow model",
-    long_description=readme,
-    author="iSnobal Community",
-    url='https://github.com/iSnobal/pysnobal',
-    packages=find_packages(exclude=["tests"]),
-    include_package_data=True,
-    install_requires=requirements,
-    zip_safe=False,
-    keywords='pysnobal',
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: CC0 1.0',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6'
-    ],
-    test_suite='tests',
-    tests_require=test_requirements,
-    cmdclass={
-        'build_ext': build_ext
-    },
+    cmdclass={"build_ext": build_ext},
     ext_modules=extensions,
 )
