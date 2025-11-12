da7e4314ce4867d3b851a4eb770fd5c2849922ef
Joachim Meyer <jmeyer@boisestate.edu>
2025-11-11 18:52:58 -0700

    _h_le - Further refine error output; format code comments

--- pysnobal/c_snobal/libsnobal/_h_le.c
+++ pysnobal/c_snobal/libsnobal/_h_le.c
@@ -2,12 +2,6 @@
  ** NAME
  **      _h_le -- calculates turbulent transfer at a point
  **
- ** SYNOPSIS
- **      #include "_snobal.h"
- **
- **      int
- **	_h_le(void)
- **
  ** DESCRIPTION
  **      Calculates point turbulent transfer (H and L_v_E) for a 2-layer
  **      snowcover.
@@ -22,28 +16,23 @@
 #include "envphys.h"
 #include "error_logging.h"
 
-/**
-
-@param e_s
-@param sat_vp   Saturation vapor pressure
-@param rel_z_T  Relative temperature measurement height above snow surface
-@param rel_z_u  Relative wind speed measurement height above snow surface
-
-@return
-*/
 int _h_le(void) {
+    // Saturation Vapor Pressure at surface temperature
     double e_s;
+    // Saturation Vapor Pressure at air temperature
     double sat_vp;
+    // Relative temperature measurement height above snow surface
     double rel_z_T;
+    // Relative wind speed measurement height above snow surface
     double rel_z_u;
     LoopResult hle1_result;
 
-    /* calculate saturation vapor pressure */
+    // Calculate saturation vapor pressure
     e_s = sati(T_s_0);
     if (e_s == FALSE)
         return FALSE;
 
-    /*** error check for bad vapor pressures ***/
+    // Error check for vapor pressures
     sat_vp = sati(T_a);
     if (sat_vp == FALSE)
         return FALSE;
@@ -51,7 +40,7 @@ int _h_le(void) {
         e_a = sat_vp;
     }
 
-    /* determine relative measurement heights */
+    // Determine if heights are relative or absolute
     if (relative_hts) {
         rel_z_T = z_T;
         rel_z_u = z_u;
@@ -60,17 +49,17 @@ int _h_le(void) {
         rel_z_u = z_u - z_s;
     }
 
-    /* calculate H & L_v_E */
+    // Calculate H & L_v_E
     hle1_result = hle1(P_a, T_a, T_s_0, rel_z_T, e_a, e_s, rel_z_T, u, rel_z_u, z_0, &H, &L_v_E, &E);
     if (hle1_result.return_code != 0) {
         LOG_ERROR(
             "hle1 did not converge \n"
-            "P_a: %f \n "
-            "T_a: %f \t "
-            "T_s_0: %f \n "
-            "e_a: %f \t "
-            "e_s: %f \n "
-            "u: %f \n"
+            "Air Pressure (P_a): %f \n "
+            "Air Temperature (T_a): %f \t "
+            "Snow Surface Temperature (T_s_0): %f \n "
+            "Vapor Pressure (e_a): %f \t "
+            "Saturation Vapor Pressure (e_s): %f \n "
+            "Wind Speed (u): %f \n"
             "last difference: %f",
             P_a, T_a, T_s_0, e_a, e_s, u, z_0, hle1_result.remainder
         );
