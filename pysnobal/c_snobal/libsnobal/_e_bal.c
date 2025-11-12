292dde2533bd372d03e642712bf8f6ea1ae086da
Joachim Meyer <jmeyer@boisestate.edu>
2025-11-11 19:55:14 -0700

    Code format and removal of commented out code
    This run clang-format from call_snobal.c to _ebal.c and removed all commented out code.
    Most of the code was leftover debugging using `fprint`. Others were inactive conditionals
    to output intermediate time steps.
    
    This is a first step towards making the code a bit more user readable. Still lots to do.

--- pysnobal/c_snobal/libsnobal/_e_bal.c
+++ pysnobal/c_snobal/libsnobal/_e_bal.c
@@ -14,74 +14,51 @@
  ** RETURN VALUE
  **
  **	TRUE	The calculations were completed.
- **
- **	FALSE	An error occured, and a message explaining the error has
- **		been stored with the 'usrerr' routine.
- **
- ** GLOBAL VARIABLES READ
- **
- ** GLOBAL VARIABLES MODIFIED
+ **	FALSE	An error occurred
  **
  */
 
-//#include        "ipw.h"
-#include        "_snobal.h"
-#include        "snow.h"
+#include "_snobal.h"
+#include "snow.h"
 
-int    
-_e_bal(void)
-{
-	if (snowcover) {
+int _e_bal(void) {
+    if (snowcover) {
+        /**	Calculate energy transfer terms  **/
+        // Net radiation
+        _net_rad();
 
-		/**	calculate energy xfer terms  **/
+        // Calculate H, L_v_E, E as well
+        if (!_h_le())
+            return FALSE;
 
-		/*      calculate net radiation */
+        // G & G_0 (conduction/diffusion heat transfer)
+        if (layer_count == 1) {
+            G = g_soil(rho, T_s_0, T_g, z_s_0, z_g, P_a);
+            G_0 = G;
+        } else { // layer_count == 2
+            G = g_soil(rho, T_s_l, T_g, z_s_l, z_g, P_a);
+            G_0 = g_snow(rho, rho, T_s_0, T_s_l, z_s_0, z_s_l, P_a);
+        }
 
-		_net_rad();
+        // Calculate advection
+        _advec();
 
-		/*      calculate H & L_v_E  (and E as well)       */
+        /** Sum energy balance terms **/
+        // Surface energy budget
+        delta_Q_0 = R_n + H + L_v_E + G_0 + M;
 
-		if (! _h_le())
-			return FALSE;
+        // Total snowpack energy budget
+        if (layer_count == 1)
+            delta_Q = delta_Q_0;
+        else // layer_count == 2
+            delta_Q = delta_Q_0 + G - G_0;
+    } else {
+        R_n = 0.0;
+        H = L_v_E = E = 0.0;
+        G = G_0 = 0.0;
+        M = 0.0;
+        delta_Q = delta_Q_0 = 0.0;
+    }
 
-		/*      calculate G & G_0(conduction/diffusion heat xfr)    */
-
-		if (layer_count == 1) {
-			G = g_soil (rho, T_s_0, T_g, z_s_0, z_g, P_a);
-			G_0 = G;
-		}
-		else {  /*  layer_count == 2  */
-			G = g_soil (rho, T_s_l, T_g, z_s_l, z_g, P_a);
-			G_0 = g_snow (rho, rho, T_s_0, T_s_l, z_s_0, z_s_l,
-					P_a);
-		}
-
-		/*      calculate advection     */
-
-		_advec();
-
-		/*      sum E.B. terms  */
-
-		/* surface energy budget */
-		delta_Q_0 = R_n + H + L_v_E + G_0 + M;
-
-		/* total snowpack energy budget */
-		if (layer_count == 1)
-			delta_Q = delta_Q_0;
-		else  /* layer_count == 2 */
-			delta_Q = delta_Q_0 + G - G_0;
-	}
-	else {
-		R_n = 0.0;
-
-		H = L_v_E = E = 0.0;
-
-		G = G_0 = 0.0;
-
-		M = 0.0;
-
-		delta_Q = delta_Q_0 = 0.0;
-	}
-
-	return TRUE;
+    return TRUE;
 }
